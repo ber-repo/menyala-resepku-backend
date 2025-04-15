766db4208cbd13edf64843f6a76ee8a8694325b4
prasetyo2k22 <ekoprasetyo@smkwiraharapan.sch.id>
2025-04-15 10:04:03 +0800

    Create IngredientRepository.java
    repository

---
+++ src/main/java/com/foodrecipe/backend/repository/IngredientRepository.java
@@ -0,0 +1,10 @@
+package com.foodrecipe.backend.repository;
+
+import com.foodrecipe.backend.model.Ingredient;
+import org.springframework.data.jpa.repository.JpaRepository;
+
+import java.util.List;
+
+public interface IngredientRepository extends JpaRepository<Ingredient, Integer> {
+    Ingredient findByIngredient(String name);
+}
