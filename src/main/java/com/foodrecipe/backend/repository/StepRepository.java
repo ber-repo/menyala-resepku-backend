0c46147038572ee99cd5cec807c299465f72ea18
dhipapradnya <162403046+dhipapradnya@users.noreply.github.com>
2025-04-29 09:19:09 +0800

    Create StepRepository.java

---
+++ src/main/java/com/foodrecipe/backend/repository/StepRepository.java
@@ -0,0 +1,11 @@
+package com.foodrecipe.backend.repository;
+
+import com.foodrecipe.backend.model.Ingredient;
+import com.foodrecipe.backend.model.Step;
+import org.springframework.data.jpa.repository.JpaRepository;
+
+import java.util.List;
+
+public interface StepRepository extends JpaRepository<Step, Integer> {
+    List<Step> findByRecipeIdOrderByOrderNumber(Integer recipeId);
+}
