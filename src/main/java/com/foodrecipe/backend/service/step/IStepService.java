3787a265c270bef1d6ea0bd01130a20fd455dac3
dhipapradnya <162403046+dhipapradnya@users.noreply.github.com>
2025-04-29 09:18:06 +0800

    Create IStepService.java

---
+++ src/main/java/com/foodrecipe/backend/service/step/IStepService.java
@@ -0,0 +1,13 @@
+package com.foodrecipe.backend.service.step;
+
+import com.foodrecipe.backend.model.Step;
+
+import java.util.List;
+
+public interface IStepService {
+    Step addStep(Step step);
+    Step getStepById(Integer id);
+    List<Step> getStepByRecipeId(Integer recipeId);
+    Step updateStep(Integer id, Step stepDetails);
+    void deleteStep(Integer id);
+}
