cb763f624fdc112d5b5946b0af5ad6d132546401
dhipapradnya <162403046+dhipapradnya@users.noreply.github.com>
2025-06-20 06:12:52 +0800

    Update StepController.java

--- src/main/java/com/foodrecipe/backend/controller/StepController.java
+++ src/main/java/com/foodrecipe/backend/controller/StepController.java
@@ -1,11 +1,17 @@
 package com.foodrecipe.backend.controller;
 
+import com.foodrecipe.backend.exception.ResourceNotFoundException;
 import com.foodrecipe.backend.model.Step;
+import com.foodrecipe.backend.response.ApiResponse;
 import com.foodrecipe.backend.service.step.IStepService;
+import org.springframework.graphql.data.method.annotation.QueryMapping;
 import org.springframework.http.ResponseEntity;
 import org.springframework.web.bind.annotation.*;
 
 import java.util.List;
+
+import static org.springframework.http.HttpStatus.INTERNAL_SERVER_ERROR;
+import static org.springframework.http.HttpStatus.NOT_FOUND;
 
 @RestController
 @RequestMapping("${api.prefix}/step")
@@ -18,32 +24,56 @@ public class StepController {
     }
 
     @PostMapping("/add")
-    public ResponseEntity<Step> addStep(@RequestBody Step step) {
-        Step createdStep = stepService.addStep(step);
-        return ResponseEntity.ok(createdStep);
+    public ResponseEntity<ApiResponse> addStep(@RequestBody Step step) {
+        try {
+            Step createdStep = stepService.addStep(step);
+            return ResponseEntity.ok(new ApiResponse(createdStep,"Success"));
+        } catch (ResourceNotFoundException e) {
+            return ResponseEntity.status(INTERNAL_SERVER_ERROR).body(new ApiResponse(null, e.getMessage()));
+        }
     }
 
-    @GetMapping("/step/{id}/step")
-    public ResponseEntity<Step> getStepById(@PathVariable Integer id) {
-        Step step = stepService.getStepById(id);
-        return ResponseEntity.ok(step);
+
+    @GetMapping("/{id}/step")
+    public ResponseEntity<ApiResponse> getStepById(@PathVariable Integer id) {
+        try {
+            Step step = stepService.getStepById(id);
+            return ResponseEntity.ok(new ApiResponse(step,"Success"));
+        } catch (ResourceNotFoundException e) {
+            return ResponseEntity.status(NOT_FOUND).body(new ApiResponse(null, e.getMessage()));
+        }
     }
+
 
     @GetMapping("/recipe/{recipeId}")
-    public ResponseEntity<List<Step>> getStepsByRecipeId(@PathVariable Integer recipeId) {
-        List<Step> steps = stepService.getStepByRecipeId(recipeId);
-        return ResponseEntity.ok(steps);
+    public ResponseEntity<ApiResponse> getStepsByRecipeId(@PathVariable Integer recipeId) {
+        try {
+            List<Step> steps = stepService.getStepByRecipeId(recipeId);
+            return ResponseEntity.ok(new ApiResponse(steps,"Success"));
+        } catch (ResourceNotFoundException e) {
+            return ResponseEntity.status(NOT_FOUND).body(new ApiResponse(null, e.getMessage()));
+        }
     }
+
 
     @PutMapping("/{id}")
-    public ResponseEntity<Step> updateStep(@PathVariable Integer id, @RequestBody Step stepDetails) {
-        Step updatedStep = stepService.updateStep(id, stepDetails);
-        return ResponseEntity.ok(updatedStep);
+    public ResponseEntity<ApiResponse> updateStep(@PathVariable Integer id, @RequestBody Step stepDetails) {
+        try {
+            Step updatedStep = stepService.updateStep(id, stepDetails);
+            return ResponseEntity.ok(new ApiResponse(updatedStep,"Success"));
+        } catch (ResourceNotFoundException e) {
+            return ResponseEntity.status(NOT_FOUND).body(new ApiResponse(null, e.getMessage()));
+        }
     }
 
+
     @DeleteMapping("/{id}")
-    public ResponseEntity<Void> deleteStep(@PathVariable Integer id) {
-        stepService.deleteStep(id);
-        return ResponseEntity.noContent().build();
+    public ResponseEntity<ApiResponse> deleteStep(@PathVariable Integer id) {
+        try {
+            stepService.deleteStep(id);
+            return ResponseEntity.ok(new ApiResponse("Remove recipe success", null));
+        } catch (ResourceNotFoundException e) {
+            return ResponseEntity.status(NOT_FOUND).body(new ApiResponse(null, e.getMessage()));
+        }
     }
 }
