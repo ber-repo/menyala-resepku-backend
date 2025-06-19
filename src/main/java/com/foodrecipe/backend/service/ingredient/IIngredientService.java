d5cf86cf1dbd56842ee6a7ed1385ef9c0991f692
prasetyo2k22 <ekoprasetyo@smkwiraharapan.sch.id>
2025-06-20 06:19:18 +0800

    Update IIngredientService.java

--- src/main/java/com/foodrecipe/backend/service/ingredient/IIngredientService.java
+++ src/main/java/com/foodrecipe/backend/service/ingredient/IIngredientService.java
@@ -1,14 +1,19 @@
 package com.foodrecipe.backend.service.ingredient;
 
 import com.foodrecipe.backend.model.Ingredient;
+import com.foodrecipe.backend.model.Recipe;
 
 import java.util.List;
 
 public interface IIngredientService {
     Ingredient getIngredientById(Integer id);
-    Ingredient getIngredientByIngredient(String ingredient);
+    Ingredient getIngredientByName(String name);
     List<Ingredient> getAllIngredient();
     Ingredient addIngredient(Ingredient ingredient);
+
+    Ingredient save(Ingredient ingredient);
+
     Ingredient updateIngredient(Ingredient ingredient, Integer id);
     void deleteIngredient(Integer id);
+
 }
