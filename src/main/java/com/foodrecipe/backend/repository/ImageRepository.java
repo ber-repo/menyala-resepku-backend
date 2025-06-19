600c267d8a98991ca07f186a2f59251bd6b2a1a9
Excitemax <94905481+Excitemax@users.noreply.github.com>
2025-06-20 06:10:28 +0800

    Update ImageRepository.java

--- src/main/java/com/foodrecipe/backend/repository/ImageRepository.java
+++ src/main/java/com/foodrecipe/backend/repository/ImageRepository.java
@@ -3,5 +3,10 @@ package com.foodrecipe.backend.repository;
 import com.foodrecipe.backend.model.Image;
 import org.springframework.data.jpa.repository.JpaRepository;
 
+import java.util.List;
+import java.util.Optional;
+
 public interface ImageRepository extends JpaRepository<Image, Integer> {
+    List<Image> findByRecipeId(Integer id);
+    Optional<Image> findByDownloadUrl(String downloadUrl);
 }
