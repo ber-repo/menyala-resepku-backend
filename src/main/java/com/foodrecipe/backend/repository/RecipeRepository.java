822c9c5f2d8c8819e27a592b72d17ea373c78d2c
ber <133905952+ber-repo@users.noreply.github.com>
2025-06-20 06:28:11 +0800

    update

--- src/main/java/com/foodrecipe/backend/repository/RecipeRepository.java
+++ src/main/java/com/foodrecipe/backend/repository/RecipeRepository.java
@@ -6,8 +6,8 @@ import org.springframework.data.jpa.repository.JpaRepository;
 import java.util.List;
 
 public interface RecipeRepository extends JpaRepository<Recipe, Integer> {
-
-
     List<Recipe> findByIsFavorite(Boolean isFavorite);
     List<Recipe> findByRecipeNameContainingIgnoreCase(String keyword);
+
+    List<Recipe> findByUserId(Integer userId);
 }
---
+++ src/main/java/com/foodrecipe/backend/repository/UserRepository.java
@@ -0,0 +1,10 @@
+package com.foodrecipe.backend.repository;
+
+import com.foodrecipe.backend.model.User;
+import org.springframework.data.jpa.repository.JpaRepository;
+
+public interface UserRepository extends JpaRepository<User, Integer>{
+    boolean existsByEmail(String email);
+
+    User findByEmail(String email);
+}
