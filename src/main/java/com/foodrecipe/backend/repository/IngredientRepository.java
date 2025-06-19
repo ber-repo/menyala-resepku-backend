255fb0a8e607c49f641f187548e386971159b3ae
prasetyo2k22 <ekoprasetyo@smkwiraharapan.sch.id>
2025-06-20 06:18:56 +0800

    Update IngredientRepository.java

--- src/main/java/com/foodrecipe/backend/repository/IngredientRepository.java
+++ src/main/java/com/foodrecipe/backend/repository/IngredientRepository.java
@@ -5,7 +5,10 @@ import org.springframework.data.jpa.repository.JpaRepository;
 
 import java.util.List;
 
+
 public interface IngredientRepository extends JpaRepository<Ingredient, Integer> {
-    Ingredient findByIngredient(String name);
-    boolean existsByIngredient(String ingredient);
+    Ingredient findByIngredientName(String ingredientName);
+    boolean existsByIngredientName(String ingredientName);
+
+    List<Ingredient> findByRecipeId(Integer id);
 }
