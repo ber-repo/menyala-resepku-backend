ce4fefa4cb1c59b35e5bfaab082eddf54855e24c
ber <133905952+ber-repo@users.noreply.github.com>
2025-06-20 06:43:29 +0800

    update

--- src/main/java/com/foodrecipe/backend/FoodRecipeBackendApplication.java
+++ src/main/java/com/foodrecipe/backend/FoodRecipeBackendApplication.java
@@ -2,12 +2,28 @@ package com.foodrecipe.backend;
 
 import org.springframework.boot.SpringApplication;
 import org.springframework.boot.autoconfigure.SpringBootApplication;
+import org.springframework.context.annotation.Bean;
+import org.springframework.web.servlet.config.annotation.CorsRegistry;
+import org.springframework.web.servlet.config.annotation.WebMvcConfigurer;
 
 @SpringBootApplication
 public class FoodRecipeBackendApplication {
 
 	public static void main(String[] args) {
 		SpringApplication.run(FoodRecipeBackendApplication.class);
+	}
+
+	@Bean
+	public WebMvcConfigurer corsConfigurer() {
+		return new WebMvcConfigurer() {
+			@Override
+			public void addCorsMappings(CorsRegistry registry) {
+				registry.addMapping("/graphql")
+						.allowedOrigins("*")
+						.allowedMethods("GET", "POST", "PUT", "DELETE", "OPTIONS")
+						.allowedHeaders("*");
+			}
+		};
 	}
 
 }
