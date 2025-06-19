89a5e5e6843118609b3331f1071981f2936b87c0
prasetyo2k22 <ekoprasetyo@smkwiraharapan.sch.id>
2025-06-20 06:17:26 +0800

    Update IngredientController.java

--- src/main/java/com/foodrecipe/backend/controller/IngredientController.java
+++ src/main/java/com/foodrecipe/backend/controller/IngredientController.java
@@ -6,6 +6,7 @@ import com.foodrecipe.backend.exception.ResourceNotFoundException;
 import com.foodrecipe.backend.model.Ingredient;
 import com.foodrecipe.backend.response.ApiResponse;
 import com.foodrecipe.backend.service.ingredient.IngredientService;
+import org.springframework.graphql.data.method.annotation.QueryMapping;
 import org.springframework.http.HttpStatus;
 import org.springframework.http.ResponseEntity;
 import org.springframework.web.bind.annotation.*;
@@ -24,14 +25,16 @@ public class IngredientController {
     }
 
     @GetMapping("/all")
-    public ResponseEntity<ApiResponse> getAllIngredient(){
-        try{
+    public ResponseEntity<ApiResponse> getAllIngredient() {
+        try {
             List<Ingredient> ingredient = ingredientService.getAllIngredient();
             return ResponseEntity.ok(new ApiResponse(ingredient, "found"));
-        }catch (Exception e){
-            return ResponseEntity.status(INTERNAL_SERVER_ERROR).body(new ApiResponse(INTERNAL_SERVER_ERROR, "Error:"));
+        } catch (Exception e) {
+            return ResponseEntity.status(HttpStatus.INTERNAL_SERVER_ERROR)
+                    .body(new ApiResponse(null, "Error: " + e.getMessage()));
         }
     }
+
 
     @PostMapping("/add")
     public ResponseEntity<ApiResponse> addIngredient(@RequestBody Ingredient name){
@@ -44,7 +47,8 @@ public class IngredientController {
 
     }
 
-    @GetMapping("/ingredient/{id}/ingredient")
+
+    @GetMapping("id/{id}")
     public ResponseEntity<ApiResponse> getIngredientById(@PathVariable Integer id){
         try {
             Ingredient theIngredient = ingredientService.getIngredientById(id);
@@ -54,17 +58,17 @@ public class IngredientController {
         }
     }
 
-    @GetMapping("ingredient/{name}/ingredient")
+    @GetMapping("/name/{name}")
     public ResponseEntity<ApiResponse> getIngredientByName(@PathVariable String name){
         try {
-            Ingredient theIngredient = ingredientService.getIngredientByIngredient(name);
+            Ingredient theIngredient = ingredientService.getIngredientByName(name);
             return ResponseEntity.ok(new ApiResponse(theIngredient, "Found"));
         }catch(ResourceNotFoundException e){
             return ResponseEntity.status(NOT_FOUND).body(new ApiResponse(null, e.getMessage()));
         }
     }
 
-    @DeleteMapping("ingredient/{id}/delete")
+    @DeleteMapping("/{id}/delete")
     public ResponseEntity<ApiResponse> deleteIngredient(@PathVariable Integer id){
         try {
             ingredientService.deleteIngredient(id);
@@ -74,7 +78,7 @@ public class IngredientController {
         }
     }
 
-    @PutMapping("ingredient/{id}/update")
+    @PutMapping("/{id}/update")
     public ResponseEntity<ApiResponse> updateIngredient(@PathVariable Integer id, @RequestBody Ingredient ingredient){
         try {
             Ingredient updateIngredient = ingredientService.updateIngredient(ingredient, id);
